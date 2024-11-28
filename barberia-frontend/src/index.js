f5a8fbf1fcb1b7ae2b226c90900a1b6637bb3962
Mig.G <mig.garate@duocuc.cl>
2024-11-28 01:39:54 -0300

    ajustes front

--- barberia-frontend/src/index.js
+++ barberia-frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css'; // Tus estilos globales
+import App from './App'; // Tu componente principal
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') // Montar en el contenedor con id="root"
 );
